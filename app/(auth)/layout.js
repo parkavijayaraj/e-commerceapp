30735be16fd9660b5cce0b3d1808d05d3f15b818
parkavi <parkavij08@gmail.com>
2026-03-28 16:18:26 +0530

    update

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -2,7 +2,7 @@ export default function AuthLayout({ children }) {
   return (
     <div className="flex h-screen">
 
-      {/* Right Side */}
+      
       <div className="w-1/2 flex items-center justify-center">
         {children}
       </div>
